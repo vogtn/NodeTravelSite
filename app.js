3a3af351cf66ef11e9fd28c3287aa52c5ad3a196
vogtn <vogtn@uw.edu>
2016-09-05 12:44:17 -0700

    adding qa test for about page

--- app.js
+++ app.js
@@ -21,9 +21,12 @@ app.use(function(req, res, next){
 app.get('/', function(req,res){
   res.render('home');
 });
+
 app.get('/about', function(req,res){
-  var randomFortune = fortunes[Math.floor(Math.random() * fortunes.length)];
-  res.render('about', {fortune: randomFortune});
+  res.render('about', {
+    fortune: fortune.getFortune(),
+    pageTestScript: '/qa/tests-about.js'
+  });
 });
 
 // 404 catch-all handler (middleware)
