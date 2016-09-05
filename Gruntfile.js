d24bc8c290f6d54b0cce00d7ec75cf6eb817977e
vogtn <vogtn@uw.edu>
2016-09-05 15:33:18 -0700

    updating gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -8,4 +8,18 @@ module.exports = function(grunt){
   ].forEach(function(task){
     grunt.loadNpmTasks(task);
   });
-}
+
+  //configure plugins
+  grunt.initConfig({
+    cafemocha:{
+      all: {src: 'qa/tests-*.js', options: {ui: 'tdd'},}
+    },
+    jshint:{
+      app:['app.js', 'public/js/**/*.js',
+        'lib/**/*.js'],
+      qa:['Gruntfile.js', 'public/qa/**/*.js', 'qa/**/*.js'],
+    },
+  });
+  //register tasks
+  grunt.registerTask('default', ['cafemocha','jshint']);
+};
