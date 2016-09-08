192e46990988887307d6c22eb4ad8de509f6ab56
vogtn <vogtn@uw.edu>
2016-09-08 10:36:28 -0700

    adding view to app.js

--- app.js
+++ app.js
@@ -21,6 +21,24 @@ app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
 app.use(express.static(__dirname + '/public'));
 
+
+//Newsletter
+
+app.use(require('body-parser')());
+
+app.get('/newsletter', function(req,res){
+  //provide dummy value
+  res.render('newsletter', {csrf: 'CSRF token goes here'})
+});
+
+app.post('process', function(req,res){
+  console.log('Form (from querystring): ' + req.query.form);
+  console.log('CSRF token (from hidden form field): ' + req.body._csrf);
+  console.log('Name (from visible form field): ' + req.body.name);
+  console.log('Email (from visible form field): ' + req.body.email);
+  res.redirect(303, '/thank-you');
+});
+
 //Test (middleware)
 app.use(function(req, res, next){
   res.locals.showTests = app.get('env') !== 'production' &&
